1ea327de55922a268b3f6dab25f2d718eb1f188e
Robin <78075471+Robin-Douet@users.noreply.github.com>
2021-05-17 15:37:22 +0200

    Update Script_acqui_envoi.py
    Script fonctionnelle sur la raspbery mais manque toujours la connection à la base de donnée mysql.

--- Script_acqui_envoi.py
+++ Script_acqui_envoi.py
@@ -5,16 +5,16 @@ Création: Robin DOUET
 Date: 19/03/2021
 """
 
-
 # Imports
 import sqlite3
 from time import sleep
 import serial
 import mysql.connector
 
+
 # Class
 class frame_manager:
-# ---------------------------------------- CONSTRUCTOR ------------------------------------------------ #
+    # ---------------------------------------- CONSTRUCTOR ------------------------------------------------ #
     def __init__(self):
         self.co2 = None
         self.cov = None
@@ -24,12 +24,12 @@ class frame_manager:
         self.pm2 = None
         self.pm10 = None
 
-
-# -------------------------- GETTING EVERY INFORMATIONS INSIDE THE FRAME ------------------------------ #
+    # -------------------------- GETTING EVERY INFORMATIONS INSIDE THE FRAME ------------------------------ #
     def get_data(self):
-        serialPort = serial.Serial("/dev/ttyAMA0", 57600, timeout=0.1)  # serial.Serial(/dev/ttyAMA0 , baudrate, timeout=Y)
+        serialPort = serial.Serial("/dev/ttyAMA0", 57600,
+                                   timeout=0.1)  # serial.Serial(/dev/ttyAMA0 , baudrate, timeout=Y)
 
-# ----------------------------------RESTART THE VALUES OF DATA -----------------------------------------#
+        # ----------------------------------RESTART THE VALUES OF DATA -----------------------------------------#
         self.co2 = None
         self.cov = None
         self.hum = None
@@ -46,11 +46,11 @@ class frame_manager:
                     frame = serialPort.read(serialPort.inWaiting())
                     print('\n\nTrame brut: ', frame)
                     print('Trame en hexa: ', frame.hex())
-                    idSender = frame[11:11 + 4] # recuperation of the id sender
+                    idSender = frame[11:11 + 4]  # recuperation of the id sender
                     print('Lecture de l\'ID Sender: ', idSender)
 
                     # We check if the id of the sensor are inside the frame received and we show data
-            # ------------------- GETTING THE CO2, HUMIDITY & TEMPERATURE ------------------- #
+                    # ------------------- GETTING THE CO2, HUMIDITY & TEMPERATURE ------------------- #
                     if idSender == b'\xff\xd5\xa8\x0a':
                         print('C02:', frame[8] * 10, 'ppm')
                         print('Humidite:', frame[7] / 2, '%')
@@ -59,13 +59,13 @@ class frame_manager:
                         self.hum = frame[7] / 2
                         self.temp = frame[9] * 51 * 255
 
-            # ------------------------------ GETTING THE COV -------------------------------- #
+                    # ------------------------------ GETTING THE COV -------------------------------- #
                     if idSender == b'\xff\xd5\xa8\x0f':
                         print('COV:', frame[7] * 255 + frame[8],
                               "ppb")  # frame[7] don't really increase but the frame[8] increase his value
                         self.cov = frame[7] * 255 + frame[8]
 
-            # ----------------------- GETTING THE PM1, PM2.5, PM10 -------------------------- #
+                    # ----------------------- GETTING THE PM1, PM2.5, PM10 -------------------------- #
                     if idSender == b'\xFF\xD5\xA8\x14':
                         print('PM1:', frame[7] * 2 + frame[8] // 128, 'PM2.5:', frame[8] * 4 + frame[9] // 64, 'PM10:',
                               frame[9] * 8 + frame[10] // 32)
@@ -74,28 +74,30 @@ class frame_manager:
                         self.pm10 = frame[9] * 8 + frame[10] // 32
 
                 print("Valeurs incomplète")
-                print("Co2 :", self.co2, "Hum:", self.hum, "Temp:", self.temp, "Cov:", self.cov, "PM1:", self.pm1, "PM2:", self.pm2, "PM10:", self.pm10)
+                print("Co2 :", self.co2, "Hum:", self.hum, "Temp:", self.temp, "Cov:", self.cov, "PM1:", self.pm1,
+                      "PM2:", self.pm2, "PM10:", self.pm10)
                 sleep(10)
             else:
                 return [self.co2, self.cov, self.hum, self.temp, self.pm1, self.pm2, self.pm10]
 
 
 class bdd:
-# ---------------------------------------- CONSTRUCTOR ------------------------------------------------ #
+    # ---------------------------------------- CONSTRUCTOR ------------------------------------------------ #
     def __init__(self):
         self.connexion_sqlite = None
         self.connexion_mysql = None
 
-# ------------------------------ CONNECTION TO THE DATABASE SQLITE ------------------------------------ #
+    # ------------------------------ CONNECTION TO THE DATABASE SQLITE ------------------------------------ #
     def connection_bdd_sqlite(self):
         self.connexion_sqlite = sqlite3.connect('/var/www/html/adminer/bdd_sondes.db')
 
-# ------------------------------ CONNECTION TO THE DATABASE MYSQL ------------------------------------- #
+    # ------------------------------ CONNECTION TO THE DATABASE MYSQL ------------------------------------- #
     def connection_bdd_mysql(self):
-        self.connexion_mysql = mysql.connector.connect(user='cq2a2021', password='EwFGFH12RErn6fM', host='https://phpmyadmin-gra1.hosting.ovh.net/by93828-001.privatesql',
+        self.connexion_mysql = mysql.connector.connect(user='cq2a2021', password='EwFGFH12RErn6fM',
+                                                       host='https://phpmyadmin-gra1.hosting.ovh.net/by93828-001.privatesql',
                                                        database='cq2a2021', port='35146')
 
-# -------------------------------- UPDATING THE DATABASE SQLITE --------------------------------------- #
+    # -------------------------------- UPDATING THE DATABASE SQLITE --------------------------------------- #
     def set_bdd_sqlite(self, val_c02, val_cov, val_humi, val_temp, val_pm1, val_pm2, val_pm10):
         cursor = self.connexion_sqlite.cursor()
         update_val = 'UPDATE donnees_sondes SET co2 = ?, cov = ?, humidite = ?, temperature = ?, pm1 = ?, pm2 = ?, pm10 = ? WHERE id = 1'
@@ -104,8 +106,8 @@ class bdd:
         self.connexion_sqlite.commit()
         print("Mise a jour de la base de donnees SQLite reussi !")
         cursor.close()
-        
-# ------------------------- CREATING A NEW ENTRY IN THE DATABASE MYSQL -------------------------------- #
+
+    # ------------------------- CREATING A NEW ENTRY IN THE DATABASE MYSQL -------------------------------- #
     def set_bdd_mysql(self, val_c02, val_cov, val_pm1, val_pm2, val_pm10, val_temp, val_hum):
         cursor = self.connexion_mysql.cursor()
         insert_val = "INSERT INTO qualiteairdonnees (tauxco2, tauxcov, tauxpm1, tauxpm2, tauxpm10, temperature, humidite) VALUES (%s, %s, %s, %s, %s, %s, %s)"
@@ -119,25 +121,25 @@ class bdd:
 
 # Programme principal
 def main():
-# -------------------- CREATING THE OBJECT USING THE CLASS "bdd" & "frame_manager"--------------------- #
+    # -------------------- CREATING THE OBJECT USING THE CLASS "bdd" & "frame_manager"--------------------- #
     senders = frame_manager()
     bdd_sqlite = bdd()
-    bdd_mysql = bdd()
+    #bdd_mysql = bdd()
 
-# --------------------------------- CONNECTION TO DATABASES & PORTS------------------------------------ #
-    #senders.connection_port()
+    # --------------------------------- CONNECTION TO DATABASES & PORTS------------------------------------ #
+    # senders.connection_port()
     bdd_sqlite.connection_bdd_sqlite()
-    bdd_mysql.connection_bdd_mysql()
+    #bdd_mysql.connection_bdd_mysql()
 
-# -------------------------------------SENDING THE DATA IN A LIST ------------------------------------- #
+    # -------------------------------------SENDING THE DATA IN A LIST ------------------------------------- #
 
-
-# ------------------------------- SENDING THE DATA LIST TO THE DATABASES -------------------------------#
+    # ------------------------------- SENDING THE DATA LIST TO THE DATABASES -------------------------------#
     while True:
         data = senders.get_data()
-        bdd_sqlite.set_bdd_sqlite(data[0], data[1], data[2], data[3], data[4], data[5], data[6]) #co2 - cov - humidite - temperature - pm1 - pm2 - pm10
-        bdd_mysql.set_bdd_mysql(data[0], data[1], data[4], data[5], data[6], data[3], data[2]) #co2 - cov - pm1 - pm2 - pm10 - temperature - humidite
-        print(data,"\n")
+        bdd_sqlite.set_bdd_sqlite(data[0], data[1], data[2], data[3], data[4], data[5],
+                                  data[6])  # co2 - cov - humidite - temperature - pm1 - pm2 - pm10
+        #bdd_mysql.set_bdd_mysql(data[0], data[1], data[4], data[5], data[6], data[3], data[2])  # co2 - cov - pm1 - pm2 - pm10 - temperature - humidite
+        print(data, "\n")
         sleep(30)
         print("\n==================================================================\n")
 
@@ -145,3 +147,4 @@ def main():
 if __name__ == '__main__':
     main()
 # Fin
+
